5f9cabacfb5dd0fbd10c587176683b3d3db3e812
codestation404@gmail.com <codestation404@gmail.com@92433fd6-7e4b-bb86-aaf4-1020929bf1d2>
2011-07-11 14:42:51 +0000

    Initial import

---
+++ trunk/blacklist.h
@@ -0,0 +1,31 @@
+/*
+ * This file is part of ZeroRCO Patcher.
+
+ * ZeroRCO Patcher is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * ZeroRCO Patcher is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ZeroRCO Patcher. If not, see <http://www.gnu.org/licenses/ .
+ */
+
+#ifndef BLACKLIST_H_
+#define BLACKLIST_H_
+
+#include <string.h>
+#include "logger.h"
+
+//Global for blacklisting
+int g_blacklisted;
+
+void zeroCtrlSetBlackListItems(char *item, char *list[], int nitems);
+int zeroCtrlIsBlacklistedFound(void);
+
+
+#endif
---
+++ trunk/logger.c
@@ -0,0 +1,50 @@
+/*
+ * This file is part of ZeroRCO Patcher.
+
+ * ZeroRCO Patcher is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * ZeroRCO Patcher is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ZeroRCO Patcher. If not, see <http://www.gnu.org/licenses/ .
+ */
+
+#include <pspsdk.h>
+#include <pspiofilemgr.h>
+#include "logger.h"
+
+#ifdef DEBUG
+
+char _buffer_log[256];
+SceUID debug_fd = -1;
+
+void zeroCtrlInitDebug() {
+	debug_fd = sceIoOpen("ms0:/seplugins/RCO/debug_user.txt", PSP_O_CREAT | PSP_O_APPEND | PSP_O_WRONLY, 0777);
+}
+
+int kwrite(const char *path, void *buffer, int buflen) {
+    int written = 0;
+    int close = 0;
+    int k1 = pspSdkSetK1(0);
+    if(debug_fd < 0) {
+    	zeroCtrlInitDebug();
+    	close = 1;
+    }
+    if(debug_fd >= 0) {
+        written = sceIoWrite(debug_fd, buffer, buflen);
+        if(close) {
+        	sceIoClose(debug_fd);
+        	debug_fd = -1;
+        }
+    }
+    pspSdkSetK1(k1);
+    return written;
+}
+
+#endif
