93f18044dc7915f7f95261d40ed7e34ec19ec94b
codestation404@gmail.com <codestation404@gmail.com@92433fd6-7e4b-bb86-aaf4-1020929bf1d2>
2011-08-03 21:14:11 +0000

    Testing branch of r8 for testing in multiple HEN (M33/GEN/PRO/ME/TN)

---
+++ branches/testing/blacklist.c
@@ -0,0 +1,53 @@
+/*
+ * This file is part of ZeroVSH Patcher.
+
+ * ZeroVSH Patcher is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * ZeroVSH Patcher is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ZeroVSH Patcher. If not, see <http://www.gnu.org/licenses/ .
+ */
+
+//Our header
+#include "blacklist.h"
+
+
+//Global for blacklisting
+int g_blacklisted = 0;
+
+
+//OK
+void zeroCtrlSetBlackListItems(char *item, char *list[], int nitems)
+{
+	int i;
+
+	zeroCtrlWriteDebug("Blacklist items: %d\n", nitems);
+
+	for(i = 0; i < nitems; i++)
+	{
+		zeroCtrlWriteDebug("-> Item %d: %s\n", i + 1, list[i]);
+
+		if(!strcmp(item, list[i]))
+		{			
+			g_blacklisted = 1;
+		}
+	}	
+}
+//OK
+int zeroCtrlIsBlacklistedFound(void)
+{
+	if(g_blacklisted == 1)
+	{
+		zeroCtrlWriteDebug("Found\n\n");
+		return 1;
+	}
+
+	return 0;
+}
---
+++ branches/testing/build_debug.bat
@@ -0,0 +1,3 @@
+make clean
+make DEBUG=1
+pause
